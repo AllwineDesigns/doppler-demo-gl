bb3af2872d0784885450fc3bb9f3c4fe530185b7
John Allwine <jallwine86@gmail.com>
2022-06-04 19:52:27 -0600

    add callback when webgl context is lost that reloads the page Ideally, we'd figure out why context is being lost and fix it, but this seems like a brute force way to ensure the exhibit keeps working when it is lost.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrthographicCamera } from '@react-three/drei';
 import './App.css';
@@ -7,9 +8,29 @@ import TouchLines from './TouchLines';
 import { ReactComponent as Ear } from './ear.svg';
 
 function App() {
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    const contextLostCallback = () => {
+      console.log("Lost context. Refreshing...");
+      document.location.reload();
+    };
+    if(canvas) {
+      canvas.addEventListener("webglcontextlost", contextLostCallback, false);
+    }
+
+    return () => { 
+      if(canvas) {
+        canvas.removeEventListener("webglcontextlost", contextLostCallback, false);
+      };
+    };
+  }, [ canvasRef ]);
+
   return (
   <>
-    <Canvas dpr={window.devicePixelRatio}>
+    <Canvas dpr={window.devicePixelRatio} ref={canvasRef}>
       <OrthographicCamera makeDefault position={[ 0, 0, 10]}/>
       <color attach="background" args={["black"]} />
       <Rings/>
